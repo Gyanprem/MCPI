c964c5ab491703659d6d9b265d429337fa0d98e2
Gyan <gyan.chandra131@gmail.com>
2024-04-06 23:12:31 +0530

    adc@switch_intr_i2clcd

--- Day12_04/Src/main.c
+++ Day12_04/Src/main.c
@@ -20,35 +20,33 @@
 #include <stdio.h>
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
-
-#include "lis3dsh.h"
+#include "adc.h"
+#include "switch.h"
 #include "i2c_lcd.h"
+
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
-	char str1[32];
-	char str2[32];
-		int ret;
-		LcdInit();
-		LIS_Data val;
+	char str[32];
+		uint16_t val;
 		SystemInit();
-		
-		
-		LIS_Init();
-		DelayMs(1000);
+		ADC_Init();
+		LcdInit();
+		SwitchInit(SWITCH);
+		LcdPuts(LCD_LINE1,"LDR ADC Demo..");
 		while(1) {
-			ret = LIS_DRdy();
-			if(ret) {
-				val = LIS_GetData();
-				sprintf(str1, "X=%d,Y=%d", val.x, val.y);
-				sprintf(str2, "Z=%d", val.z);
-				LcdPuts(LCD_LINE1,str1);
-				LcdPuts(LCD_LINE2,str2);
-				DelayMs(1000);
-			}
+			val = ADC_GetValue();
+
+			while(exti0_flag == 0)
+						;
+			sprintf(str, "ADC=%d", val);
+			LcdPuts(LCD_LINE2,str);
+			DelayMs(1000);
+			exti0_flag = 0;
 		}
 	return 0;
 }
