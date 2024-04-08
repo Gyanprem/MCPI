ac92db6acbbaef9880765ad2658e8ee0d51cb434
Gyan <gyan.chandra131@gmail.com>
2024-04-08 16:05:37 +0530

    update

--- Day12_04/Src/adc.c
+++ Day12_04/Src/adc.c
@@ -12,7 +12,7 @@ void ADC_Init(void) {
 //    1. Enable GPIO peripheral clock
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 //    2. Configure GPIO pin as analog mode (MODER = 0b11).
-	GPIOA->MODER |= 3 << GPIO_MODER_MODE0_Pos;
+	GPIOA->MODER |= 3 << GPIO_MODER_MODE1_Pos;
 //    3. Enable ADC peripheral clock
 	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 //    4. Set ADC resolution as 12-bits (CR1 RES = 0b00) -- default
@@ -20,7 +20,7 @@ void ADC_Init(void) {
 //    6. Set number of conversions in a sequence (SQR1 L(3:0) = 0b000 for 1 conversion)
 	ADC1->SQR1 |= (1-1) << ADC_SQR1_L_Pos;
 //    7. Set channel number for conversions in sequence (e.g. SQR3 (5:0) = 0b00000 for channel 0).
-	ADC1->SQR3 |= 0 << ADC_SQR3_SQ1_Pos;
+	ADC1->SQR3 |= 1 << ADC_SQR3_SQ1_Pos;
 //    8. Enable ADC (CR2 ADON=0b1)
 	ADC1->CR2 |= ADC_CR2_ADON;
 }
